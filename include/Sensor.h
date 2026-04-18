a6e03a2b19fd7903753043d871aaacf3c61c2a03
caoqingkai57 <caoqingkai57@gmail.com>
2026-04-18 18:07:29 +0100

    Refactor Sensor.h header file formatting

--- include/Sensor.h
+++ include/Sensor.h
@@ -101,6 +101,11 @@ public:
     void setDataCallback(DataCallback cb);
 
     bool checkPartID();
+/**
+     * @brief Configure sensor parameters.
+     * @note The internal 10ms sleep after reset is required by MAX30102 datasheet
+     *       and occurs only during initialization.
+     */
     bool configureSensor(SampleAverage avg = SAMPLEAVG_4,
                          SampleRate rate = SAMPLERATE_100,
                          LedPulseWidth width = PULSEWIDTH_411);
